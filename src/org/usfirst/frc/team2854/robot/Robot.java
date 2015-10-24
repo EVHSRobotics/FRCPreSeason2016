c124fea0d06d263b96c034922ff0f321d81bfd7b
Kevin Wang <wangkevin448@gmail.com>
2015-10-23 17:16:54 -0700

    oi finished

---
+++ src/org/usfirst/frc/team2854/robot/Robot.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2854.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+/**
+ * Created by Kevin on 10/23/2015.
+ */
+public class Robot extends IterativeRobot{
+
+}
